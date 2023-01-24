1c6afa6c39cf1e9b8fb06c1631370d10af73b1c2
BARDAMRI <bardam@post.bgu.ac.il>
2023-01-24 18:17:36 +0200

    add instersection method in Model.py

--- Semantle_AI/Business/Model/Model.py
+++ Semantle_AI/Business/Model/Model.py
@@ -30,7 +30,7 @@ class Model:
     def get_number_of_dim(self):
         return self.model.vector_size
 
-    def models_vocab_intersection(self, model2: Model) -> set:
+    def models_vocab_intersection(self, model2) -> set:
         vocab1 = self.get_vocab()
         vocab2 = model2.get_vocab()
         return vocab1 & vocab2
